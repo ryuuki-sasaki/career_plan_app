5a0bd8d7569cabb6d25b6bce97275f8f666b8222
ryuuki-sasaki <r.ryuuki.sasaki@gmail.com>
2023-10-31 08:17:08 +0900

    [add] Futures登録機能を追加。

--- app/controllers/futures_controller.rb
+++ app/controllers/futures_controller.rb
@@ -1,2 +1,10 @@
 class FuturesController < ApplicationController
+  def create
+    @future = Future.new(params.require(:future).permit(:future_image))
+    if @future.save
+      render json: @future, status: :created
+    else
+      render json: @future.errors, status: :unprocessable_entity
+    end
+  end
 end
--- spec/requests/futures_spec.rb
+++ spec/requests/futures_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Futures", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  # futureを作成する
+  describe "POST /create" do
+    # 正常なレスポンスを返すこと
+    it "returns a success response" do
+      post "/futures/create", params: { future: { future_image: "ラーメン店を開業する。" } }.to_json, headers: { "Content-Type" => "application/json" }
+      expect(response).to be_successful
+    end
   end
 end
