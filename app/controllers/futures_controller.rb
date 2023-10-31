738a77e2bef61eb6dc8079d095361ce2e8226f88
ryuuki-sasaki <r.ryuuki.sasaki@gmail.com>
2023-10-31 21:05:08 +0900

    [update] strong parametersは今後も同じような処理を書くことが分かっているため別関数に切り出す。

--- app/controllers/futures_controller.rb
+++ app/controllers/futures_controller.rb
@@ -1,10 +1,15 @@
 class FuturesController < ApplicationController
   def create
-    @future = Future.new(params.require(:future).permit(:future_image))
+    @future = Future.new(future_params)
     if @future.save
       render json: @future, status: :created
     else
       render json: @future.errors, status: :unprocessable_entity
     end
   end
+
+  private
+    def future_params
+      params.require(:future).permit(:future_image)
+    end
 end
